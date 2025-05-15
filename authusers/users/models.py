cc65f16e4f1e1d926cf41d4808dc1bff063faab3
murMeowCode <grisha23.01.2004@gmail.com>
2025-05-15 10:53:20 +0300

    Delete authusers directory

--- authusers/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-
-class User(AbstractUser):
-    role = models.IntegerField()
-    birthday = models.DateField(null=True)
-    father_name = models.CharField(max_length=25,null=True,verbose_name='Отчество') 
