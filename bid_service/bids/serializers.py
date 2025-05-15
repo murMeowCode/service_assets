e5312e0fbfc168928e298f45c316c1d4249151cb
murMeowCode <grisha23.01.2004@gmail.com>
2025-05-15 17:52:45 +0300

    Создан сериализатор

---
+++ bid_service/bids/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Bid
+
+class BidSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bid
+        fields = '__all__'
+        read_only_fields = ('created_at')
