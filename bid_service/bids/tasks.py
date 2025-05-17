5435694f7027771cd17ff1cdac296840ff73eec4
murMeowCode <grisha23.01.2004@gmail.com>
2025-05-17 10:20:39 +0300

    update publishing

--- bid_service/bids/tasks.py
+++ bid_service/bids/tasks.py
@@ -29,16 +29,19 @@ def send_notification(bid):
     
     # Формируем сообщение
     message = {
-        'to_user': bid.author_id,
-        'user_email': bid.author_email,  # Предполагаем, что у User есть email
-        'subject': 'Результат рассмотрения заявки',
-        'content': get_notification_content(bid.status)
+        'task': 'notify.tasks.process_notification',  # Указываем полный путь к задаче
+        'args': [{
+            'to_user': bid.author_id,
+            'user_email': bid.author_email,
+            'subject': 'Результат рассмотрения заявки',
+            'content': get_notification_content(bid.status)
+        }],
+        'kwargs': {}
     }
-    
-    # Отправляем в очередь
+
     channel.basic_publish(
-        exchange='',
-        routing_key='notifications',
+        exchange='notifications',  # Должно соответствовать обменнику в Celery
+        routing_key='notifications',  # Должно соответствовать routing_key в Celery
         body=json.dumps(message),
         properties=pika.BasicProperties(
             delivery_mode=2,  # Сообщение persistent
