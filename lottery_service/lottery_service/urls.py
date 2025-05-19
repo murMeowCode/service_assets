6cd80688defe5372fb21ea882155cd4471f2a4f4
murmeowcode <grisha23.01.2004@gmail.com>
2025-05-19 12:49:19 +0300

    Create URLs

--- lottery_service/lottery_service/urls.py
+++ lottery_service/lottery_service/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lot.views import LotteryListAPIView, ParticipantListCreateAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('lotteries/', LotteryListAPIView.as_view(), name='lottery-list'),
+    path('participants/', ParticipantListCreateAPIView.as_view(), name='participant-list-create'),
 ]
